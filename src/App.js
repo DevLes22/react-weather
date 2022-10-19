5178003ca2b91bcd6004769a43d31e40b3dc9da9
DevLes22 <alesiaveretnik97@gmail.com>
2022-10-19 20:44:06 +0300

    Axios Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Weather() {
+
+  function handleResponse(response) {
+alert(`The weather is ${response.data.main.temp}`);
+  }
+
+    let apiKey = `4e3399058fb83764b1a548f8be443cf5`;
+    let units = `metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+
+  return <h1>Hello Weather</h1>;
 }
-
-export default App;
